3df674500d2e694e2e05c7dcd52fd6184972858c
sam1610 <samir.amri@gmail.com>
2018-12-25 13:04:52 +0300

    frist Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { EmployeeService } from './shared/employee.service';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { AngularFirestoreModule } from "@angular/fire/firestore"; 
+import { environment } from 'src/environments/environment';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { FormsModule } from  "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule, FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,16 @@
+import { EmployeeService } from './../../shared/employee.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private service:EmployeeService) { }
+
+  ngOnInit() {
+  }
+
+}
